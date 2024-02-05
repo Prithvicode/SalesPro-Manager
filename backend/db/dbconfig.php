4d80699040686ea6c42887e30cddf4b5ad16586d
Prithvi <prithvighatani7@gmail.com>
2024-02-05 10:56:18 +0545

    check push

---
+++ backend/db/dbconfig.php
@@ -0,0 +1,20 @@
+<?php
+
+// DATABASE
+define ("DB_HOST", "localhost");
+define ("DB_USER", "root");
+define ("DB_PASS", "");
+define ("DB_NAME", "inventorysalesdb");
+define ("DB_PORT", 3308);
+
+$conn = mysqli_connect(DB_HOST, DB_USER , DB_PASS, DB_NAME, DB_PORT);
+
+if(!$conn){
+    echo "db Error";
+}
+
+
+
+
+
+?>
